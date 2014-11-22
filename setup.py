1c3eda941b5bb12e16ec4e1b4afd84ef6ee05540
Tobias Gläßer <tobimensch@yahoo.de>
2014-11-22 19:22:51 +0100

    added manpage and setup.py can install termsql and the manpage

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
+import sys, os, shutil
 from distutils.core import setup
-from build_manpage import BuildManPage
 
 setup(name='termsql',
       version='0.3alpha',
       description='Convert text from a file or from stdin into SQL table and query it instantly. Uses sqlite as backend. The idea is to make SQL into a tool on the command line or in scripts.',
       author='Tobias Glaesser',
       url='https://gitorious.org/termsql',
-      py_modules=['termsql'],
- cmdclass={
-        'build_manpage': BuildManPage
-    }
+      scripts=['termsql'],
+      data_files=[('/usr/share/man/man1/', ['termsql.1.gz'])]
      )
+
 
