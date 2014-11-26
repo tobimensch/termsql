8faed717d9ab1c6d6d8c4a7e66874f3745ec10cd
Tobias Gläßer <tobimensch@yahoo.de>
2014-11-26 04:45:43 +0100

    improved help/manpage

--- generatemanpage.sh
+++ generatemanpage.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 export MORE_HELP=True
-help2man -N ./termsql | gzip > termsql.1.gz
+help2man --include=manpage_additional_info --source=https://github.com/tobimensch/termsql -N ./termsql | gzip > termsql.1.gz
 man2html -r ./termsql.1.gz > termsql_manpage.html
