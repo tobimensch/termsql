6ba995d440821969f6420185b55f4cc0a94258bd
Tobias Gläßer <tobimensch@yahoo.de>
2020-04-30 20:07:56 +0200

    setup.py updates

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ import sys, os, shutil
 from distutils.core import setup
 
 setup(name='termsql',
-      version='0.3',
+      version='1.0',
       description='Convert text from a file or from stdin into SQL table and query it instantly. Uses sqlite as backend. The idea is to make SQL into a tool on the command line or in scripts.',
       author='Tobias Glaesser',
-      url='https://gitorious.org/termsql',
+      author_email='tobimensch@gmail.com',
+      url='https://github.com/tobimensch/termsql',
       scripts=['termsql','where','limit','groupby','orderby','select'],
-      data_files=[('/usr/share/man/man1/', ['termsql.1.gz'])]
+      data_files=[('/usr/share/man/man1/', ['termsql.1.gz'])],
+      license='MIT'
      )
 
 
